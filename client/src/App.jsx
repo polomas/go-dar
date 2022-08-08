cbe19c98c0b962fbe10b6d0b0d0b00f3d6ccd4cb
polomas <benavidesge@outlook.com>
2022-08-07 20:26:25 -0500

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <button
+        onClick={async () => {
+          const response = await fetch("http://localhost:3000/users");
+          const data = await response.json();
+          console.log(data);
+        }}
+      >
+        Obtener datos
+      </button>
+    </div>
+  );
+}
+
+export default App;
